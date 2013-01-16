897ef67252fba6dc2157876a697491f62a1dbf3f
Alex <alex@migame.org>
2013-01-15 18:08:41 -0600

    Added documentation, tests

--- src/main/java/org/migame/velocity/tools/HashTool.java
+++ src/main/java/org/migame/velocity/tools/HashTool.java
@@ -1,32 +1,18 @@
 package org.migame.velocity.tools;
 
 import org.apache.commons.codec.digest.DigestUtils;
-import org.apache.velocity.tools.config.DefaultKey;
 
 /**
  * @author Alex
- * @version 0.0.1
+ * @version 1.0.0
  * 
- * TODO: Document HashTool (org.migame.velocity.tools)
+ * Velocity tool which wraps several methods of {@link DigestUtils}. 
  */
-
-@DefaultKey("hash")
 public class HashTool
 {
     /**
-     * 
-     * @param data
-     * @return
-     */
-    public String md5(String data)
-    {
-        return DigestUtils.md5Hex(data);
-    }
-    
-    /**
-     * 
-     * @param data
-     * @return
+     * @param data The string to hash
+     * @return The MD2 hash of the given string as a lowercase hex string
      */
     public String md2(String data)
     {
@@ -34,9 +20,17 @@ public class HashTool
     }
     
     /**
-     *
-     * @param data
-     * @return
+     * @param data The string to hash
+     * @return The MD5 hash of the given string as a lowercase hex string
+     */
+    public String md5(String data)
+    {
+        return DigestUtils.md5Hex(data);
+    }
+    
+    /**
+     * @param data The string to hash
+     * @return The SHA1 hash of the given string as a lowercase hex string
      */
     public String sha1(String data)
     {
@@ -44,9 +38,8 @@ public class HashTool
     }
     
     /**
-     *
-     * @param data
-     * @return
+     * @param data The string to hash
+     * @return The SHA256 hash of the given string as a lowercase hex string
      */
     public String sha256(String data)
     {
@@ -54,9 +47,8 @@ public class HashTool
     }
     
     /**
-     *
-     * @param data
-     * @return
+     * @param data The string to hash
+     * @return The SHA384 hash of the given string as a lowercase hex string
      */
     public String sha384(String data)
     {
@@ -64,9 +56,8 @@ public class HashTool
     }
     
     /**
-     *
-     * @param data
-     * @return
+     * @param data The string to hash
+     * @return The SHA512 hash of the given string as a lowercase hex string
      */
     public String sha512(String data)
     {
--- src/test/java/org/migame/velocity/tools/HashToolTest.java
+++ src/test/java/org/migame/velocity/tools/HashToolTest.java
@@ -1,41 +1,51 @@
 package org.migame.velocity.tools;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 /**
  * @author Alex
- * @version TODO: version
- *          TODO: Document HashToolTest (org.migame.velocity.tools)
+ * @version 1.0.0
+ * 
+ * Tests the methods of {@link HashTool}.
  */
 public class HashToolTest
 {
+    static private final String TEST_DATA = "test@test.com";
+    private final HashTool tool;
+    
+    public HashToolTest()
+    {
+        this.tool = new HashTool();
+    }
+    
     public @Test void testMd2()
     {
-        // TODO: Implement testMd2
+        Assert.assertEquals(tool.md2(TEST_DATA), "991882b63f104d17d86db114096f73ae");
     }
-
+    
     public @Test void testMd5()
     {
-        // TODO: Implement testMd5 
+        Assert.assertEquals(tool.md5(TEST_DATA), "b642b4217b34b1e8d3bd915fc65c4452");
     }
     
     public @Test void testSha1()
     {
-        // TODO: Implement testSha1 
+        Assert.assertEquals(tool.sha1(TEST_DATA), "a6ad00ac113a19d953efb91820d8788e2263b28a");
     }
     
     public @Test void testSha256()
     {
-        // TODO: Implement testSha256 
+        Assert.assertEquals(tool.sha256(TEST_DATA), "f660ab912ec121d1b1e928a0bb4bc61b15f5ad44d5efdc4e1c92a25e99b8e44a");
     }
     
     public @Test void testSha384()
     {
-        // TODO: Implement testSha384 
+        Assert.assertEquals(tool.sha384(TEST_DATA), "49a126cb1504ba78c7de144fc6574b6f0a7dc92992fb7d4a4dadf0fcd16b8c4ec9daf8cf5381c4eda328cd0f36c6d164");
     }
     
     public @Test void testSha512()
     {
-        // TODO: Implement testSha512
+        Assert.assertEquals(tool.sha512(TEST_DATA), "045b45ad1b02b7e0de222e19c77a4e765851378a1eb2202e346cde5f979a3217e83454921dfebe3e4e27bb5cc8de91ce68ed75682bd87c1e323146d94c7a2b31");
     }
 }
